032eb86225069bc6f8d444a866912b042b122991
EMMANUEL <emmanuel.oliveira3@gmail.com>
2019-12-18 22:57:08 -0300

    implementaImpostoZ

---
+++ src/templateMethod/ImpostoZ.java
@@ -0,0 +1,65 @@
+package templateMethod;
+
+import chainOfResponsibility.*;
+import java.util.*;
+
+public class ImpostoZ extends TemplateImpostoCondicional{
+
+	@Override
+	public boolean deveUsarMaximaTaxacao(Orcamento orcamento) {
+        //regra de Negócio
+		return possuiMaisUmItemMesmoNome(orcamento) || possuiMaisUmItemMesmoNomeOutraImplementacao(orcamento);
+    }
+    
+
+
+    boolean possuiMaisUmItemMesmoNomeOutraImplementacao(Orcamento orcamento){
+
+        List<String> listaItensOrcamento = new ArrayList<String>();
+
+        boolean possui = false;
+
+        for(Item item : orcamento.getItens()){
+
+            if(listaItensOrcamento.contains(item.getNome())) 
+                return true;
+            else
+                listaItensOrcamento.add(item.getNome()); 
+        }    
+
+        return possui;
+    }
+
+
+    boolean possuiMaisUmItemMesmoNome(Orcamento orcamento){
+
+        boolean possui = false;
+
+        for(int i=0; i< orcamento.getItens().size(); i ++){
+            for(int j=i+1; i<orcamento.getItens().size(); i++){
+                if(orcamento.getItens().get(i).getNome() == orcamento.getItens().get(j).getNome())
+                    possui = true;
+            }
+               
+        }    
+
+        return possui;
+    }
+
+
+
+	@Override
+	public double maximaTaxacao(Orcamento orcamento) {
+		return orcamento.getValor() * 0.13 + 100;
+	}
+
+	@Override
+	public double minimaTaxacao(Orcamento orcamento) {
+		return orcamento.getItens().size() * 0.01;
+	}
+
+
+
+
+}
+
