e4f9245080eeecbd5838a98a561de58fe8bd7bb6
EMMANUEL <emmanuel.oliveira3@gmail.com>
2019-12-21 09:46:18 -0300

    Faz uso de reflexão para apenas instanciar os objetos uma única vez

--- src/flyweight/NotasMusicais.java
+++ src/flyweight/NotasMusicais.java
@@ -1,26 +1,40 @@
 package flyweight;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class NotasMusicais{
 
     private static Map<String, Nota> notas = new HashMap<String, Nota>();
+    private static List<Class<? extends Nota>> classes;
 
     static{
 
-        notas.put("Do", new Do());
-        notas.put("Ré", new Re());
-        notas.put("Mi", new Mi());
-        notas.put("Fá", new Fa());
-
-
+        classes = Arrays.asList(Do.class,Re.class, Mi.class, Fa.class);
 
     }
-
         
 
     public Nota getNota(String nota){
+
+        if(!notas.containsKey(nota)){
+
+            for(Class<? extends Nota> classe : classes){
+
+                if(classe.getSimpleName().toLowerCase().equals(nota.toLowerCase())){
+                    try{
+                    notas.put(nota, classe.newInstance());
+                    }catch(Exception  e){
+                        throw new RuntimeException(e);
+                    }
+                    break;
+                }
+            }
+
+        }
+
         return notas.get(nota);
     }
 
