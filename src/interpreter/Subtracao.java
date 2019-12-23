a20fbf2489725a2cadd455ca5005f02df65707f9
EMMANUEL <emmanuel.oliveira3@gmail.com>
2019-12-23 09:58:12 -0300

    Implementa um visitor para imprimir expressão pré-fixa

--- src/interpreter/Subtracao.java
+++ src/interpreter/Subtracao.java
@@ -58,7 +58,4 @@ public class Subtracao implements Expressao {
     }
 
 
-
-
-
 }
---
+++ src/visitor/ImpressoraPreFixa.java
@@ -0,0 +1,81 @@
+package visitor;
+
+import interpreter.*;
+import visitor.*;
+
+
+public class ImpressoraPreFixa implements Visitor{
+
+   @Override
+   public void visitaSoma(Soma soma){
+
+        System.out.print(" + ");
+        System.out.print("(");
+        soma.getEsquerda().aceita(this);
+        soma.getDireita().aceita(this);
+        System.out.print(")");
+
+    }
+
+    @Override
+    public void visitaRaizQuadrada(RaizQuadrada raiz){
+
+        System.out.print(" ^ 2");
+        System.out.print("(");
+        raiz.getExpressao().aceita(this);
+        System.out.print(")");
+
+    }
+
+    @Override
+    public void visitaSubtracao(Subtracao subtracao){
+
+        System.out.print(" - ");
+        System.out.print("(");
+        subtracao.getEsquerda().aceita(this);
+        subtracao.getDireita().aceita(this);
+        System.out.print(")");
+
+
+    }
+
+    @Override
+    public void visitaDivisao(Divisao divisao){
+
+        System.out.print(" / ");
+        System.out.print("(");
+        divisao.getEsquerda().aceita(this);
+        divisao.getDireita().aceita(this);
+        System.out.print(")");
+
+    }
+
+    @Override
+    public void visitaMultiplicacao(Multiplicacao multiplicacao){
+
+        System.out.print(" * ");
+        System.out.print("(");
+        multiplicacao.getEsquerda().aceita(this);
+        multiplicacao.getDireita().aceita(this);
+        System.out.print(")");
+
+    }
+
+
+
+    @Override
+    public void visitaNumero(Numero numero){
+
+        numero.aceita(this);
+        System.out.print("(");
+        System.out.print(")");
+
+    }
+
+
+
+
+
+
+
+}
